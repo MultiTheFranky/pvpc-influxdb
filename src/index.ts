893d31b601a0ba668cd78f394c7fae2f0fa9201a
Francisco Alamo 'Franky' Garcia <fran.minecraft.mta@gmail.com>
2023-08-19 23:12:17 +0200

    fix: env

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ import { getDayPVPCPrices, sentPricesToInflux } from './pvpc';
 const cron = process.env.CRON || '0 5 0 * * *'; // Every day at 0:05 AM by default
 
 (async () => {
-    const cronjob = new CronJob(cron, async () => {
-        try {
-        const date = new Date();
-        const prices = await getDayPVPCPrices(date);
-        await sentPricesToInflux(prices);
-    } catch (error) {
-        console.error(error);
-    }
-    });
-    cronjob.start();
+    console.log('Starting cronjob with cron', cron);
+    new CronJob(
+        cron,
+        async () => {
+            try {
+                const date = new Date();
+                const prices = await getDayPVPCPrices(date);
+                await sentPricesToInflux(prices);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        null,
+        true,
+        'Europe/Madrid'
+    );
 })();
